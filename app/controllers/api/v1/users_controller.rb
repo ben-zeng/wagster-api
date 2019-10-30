c6df7c057de142a9f4ee4055c5e01bf278006f55
Ben <ben@benzeng.co.uk>
2019-10-30 13:26:39 +0000

    added delete method and routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,25 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      render kson: @user, status: :ok
+    else
+      render kson: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @user.destroy
+    head 204
   end
 
   private
