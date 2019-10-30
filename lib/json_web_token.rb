cd0241b1a8e67850a687041c8db339ced04414eb
Ben Zeng <benzeng94@me.com>
2019-10-30 22:11:19 +0000

    resolved token decode failure

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,14 +1,17 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  # SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY = "secretkey!!!!!"
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    p token = JWT.encode(payload, SECRET_KEY,'HS256')
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY).first
+    p token
+    decoded = JWT.decode(token, SECRET_KEY, ['HS256']).first
     HashWithIndifferentAccess.new decoded
   end
+
 
 end
