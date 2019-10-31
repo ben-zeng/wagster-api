9c64ce090aec2175d3e64ce5fe63613794db7f25
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-31 23:02:27 +0000

    1 failing test

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users, only: %i[show create]
+  end
+end 
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
       resources :users, only: [:show]
     end
-  end 
+  end
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
