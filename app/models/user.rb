c15dc493f5a18300ca3291995c5f4a466b1b137b
ilimeza <ilianameza530@gmail.com>
2019-10-31 13:49:49 +0000

    profiles api now working and user can create delete update and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_secure_password
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
