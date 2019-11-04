cac7c3e1d300c5aa4cf88a4c3b3912cb0329cfbd
ilimeza <ilianameza530@gmail.com>
2019-11-04 09:56:45 +0000

    Refactor profiles controller to use JWT controllers

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ProfilesController < ApplicationController
+    before_action :set_profile, only: [:show, :update, :destroy]
+    before_action :set_user, only: [:show, :create, :update, :destroy]
+    # TODO: Decide how to show profiles to logged in users
+    #       Currently all GET /api/v1/profiles/{USER_ID} URLs are public
+    before_action :check_owner, only: [:create, :update, :destroy]
 
     def show
-      @profile = set_profile
       if @profile
         render json: @profile
       else
@@ -21,7 +25,6 @@ class Api::V1::ProfilesController < ApplicationController
     end
 
     def update
-      @profile = set_profile
       if @profile.update(profile_params)
         render json: @profile, status: :ok
       else
@@ -40,9 +43,16 @@ class Api::V1::ProfilesController < ApplicationController
         params.require(:profile).permit(:dog_name, :biography, :user_id, :picture)
     end
 
-
     def set_profile
         @profile = Profile.find_by(user_id: params[:id])
-    end 
+    end
 
+    def set_user
+      # TODO: Clean this up, it's a bit hacky
+      @user = User.find(params[:id] || params[:profile][:user_id])
+    end
+
+    def check_owner
+      head :forbidden unless @user.id == current_user&.id
+    end
 end
