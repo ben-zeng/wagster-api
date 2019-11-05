ab259f2652970c646870bd68e6d2a2c08929bafe
Ben Zeng <benzeng94@me.com>
2019-11-05 15:11:06 +0000

    addedm profile_matches db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_174453) do
+ActiveRecord::Schema.define(version: 2019_11_05_144228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_11_01_174453) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
+    t.text "accepted_profiles", default: [], array: true
+    t.text "rejected_profiles", default: [], array: true
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
