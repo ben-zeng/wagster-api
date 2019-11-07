9f729ccf1b02ae8eafc4380ad1d85f6847eed043
Ben Zeng <benzeng94@me.com>
2019-11-07 16:18:04 +0000

    temp fix unable to login

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TokensController < ApplicationController
   def create
-    @user = User.find_by_email(user_params[:email].downcase!)
+    @user = User.find_by_email(user_params[:email])
     if @user&.authenticate(user_params[:password])
       render json: {
           token: JsonWebToken.encode(user_id: @user.id),
