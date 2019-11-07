ba16174f1d847784c136d03682fabae7d80b6db4
Ben Zeng <benzeng94@me.com>
2019-11-07 15:38:31 +0000

    case insensitive emails on login and creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
+
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
+
 end
